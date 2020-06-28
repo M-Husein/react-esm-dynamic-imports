73d26728843521427b74507bf3b5727101640253
Muhamad Husein <m.husein27@gmail.com>
2020-06-29 01:54:34 +0700

    Update ImportEsm.js

--- ImportEsm.js
+++ ImportEsm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Import from './Import';
 
-export default function ImportEsm({module, children}){ // ...etc
+export default function ImportEsm({module, funcName, children}){ // ...etc
   const [Module, setModule] = React.useState();
 	const [Err, setErr] = React.useState(false);
 	
@@ -9,34 +9,35 @@ export default function ImportEsm({module, children}){ // ...etc
 		// console.log('%cuseEffect in ImportEsm','color:yellow;');
 		async function getMod(){
       try {
-				// File name to function name & store to window
-				const fileName = 'Esm_' + module.split('/').pop().replace(/.js|.jsx|.ts|.tsx|.mjs/,'');
-				let exist = window[fileName];
+				// File name to function name & store to window 
+				// const fnName = 'Esm_' + module.split('/').pop().replace(/.js|.jsx|.ts|.tsx|.mjs/,'');
+				const fnName = funcName ? funcName : 'Esm_' + module.split('/').pop().replace(/.js|.jsx|.ts|.tsx|.mjs/,'');
+				let exist = window[fnName];
 				
 				if(exist){// Use from window if available
-					setModule(exist);// setModule(window[fileName]);
+					setModule(exist);// setModule(window[fnName]);
 				}else{// Use from import module file & store to window
 					const m = await Import(module);
 					const modName = Object.keys(m);
 					// console.log(modName);// JSON.stringify(m) | .includes('TypeError:')
-					// console.log('fileName: ', fileName);
+					// console.log('fnName: ', fnName);
 					
 					if(modName && modName.length > 0){
 						if(m.default && (!modName || modName.length < 1)){// if(m.default){
 							// console.log('if modName: ', modName);
 							// console.log('if m: ', m);
-							window[fileName] = m.default;
+							window[fnName] = m.default;
 							setModule(m.default);
 						}
 						else if(m.default && modName.length > 0){
 							let mods = {...m}
-							window[fileName] = mods;
+							window[fnName] = mods;
 							setModule(mods);
 						}	
 						else{
 							// console.log('else modName: ', modName);
 							// console.log('else m: ', m);
-							window[fileName] = m[modName[0]];
+							window[fnName] = m[modName[0]];
 							setModule(m[modName[0]]);
 						}
 					}else{
